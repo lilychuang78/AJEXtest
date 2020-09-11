dbea31c16a32ace89a62dd97369a880986d5794c
lilychuang78 <lilychuang78@gmail.com>
2020-09-11 08:50:41 +0000

    writeToDocument function

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-var xhr = new XMLHttpRequest();
-var data;
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
-function setData(jsonData){
-    data = jsonData;
-    console.log(data);
-}
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
 
+function writeToDocument(type) {
+    getData(type, function(data) {
+        document.getElementById("data").innerHTML = data;
+    });
+}
